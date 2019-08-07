3295cd274c886d8fafe2925e7b2beed5e9dee9ee
spoeltl <spoeltl@chip.de>
2019-08-08 00:02:43 +0200

    refactored code awit

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm -it -v $PWD:/usr/src -w /usr/src python:3 pip install --no-cache-dir -r requirements.txt && bash
+docker run --rm -it -v $PWD:/usr/src/app python $@
--- training_data_generator.py
+++ training_data_generator.py
@@ -2,48 +2,67 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-SHOPPING_URL = 'https://www.google.com/search?q=smartphones&source=lnms&tbm=shop'
+SHOPPING_URL = 'https://www.google.com/search?q=smartphones&source=lnms&tbm=shop&start='
+OUTPUT_FILE = 'data.xlsx'
 
-req = requests.get(SHOPPING_URL)
-soup = BeautifulSoup(req.text, "lxml")
 
-j1 = soup.find_all('div', {'class': 'g'})
-j2 = [j.find_all('div', {'class': 'pslires'}) for j in j1]
+def get_items(page=0):
+    req = requests.get(SHOPPING_URL + str(page * 20))
+    soup = BeautifulSoup(req.text, "html.parser")
+    item_containers = soup.find_all('div', {'class': 'g'})
+    items = [j.find_all('div', {'class': 'pslires'}) for j in item_containers]
+    return items
 
-data = {'Name': [], 'Price': [], 'Retailer': [], 'Stars': [], 'Description': []}
 
-for i in range(len(j2)):
-    try:
-        name = j2[i][0].find_all('a')[1].text
-        data['Name'].append(name)
-    except:
-        data['Name'].append('none')
+def parse_items(data: dict, items: list):
+    for i in range(len(items)):
+        try:
+            item = items[i][0]
+        except:
+            return
+        try:
+            name = item.find_all('a')[1].text
+            data['Name'].append(name)
+        except:
+            data['Name'].append('none')
+        try:
+            price = item.find_all('div')[2].text.replace('\xa0', ' ')
+            reta = item.find_all('div')[3].text.replace('\xa0', ' ')
+            data['Price'].append(price)
+            data['Retailer'].append(reta)
+        except:
+            data['Price'].append('none')
+            data['Retailer'].append('none')
 
-    try:
-        price = j2[i][0].find_all('div')[2].text.replace('\xa0', ' ')
-        reta = j2[i][0].find_all('div')[3].text.replace('\xa0', ' ')
-        data['Price'].append(price)
-        data['Retailer'].append(reta)
-    except:
-        data['Price'].append('none')
-        data['Retailer'].append('none')
+        try:
+            desc = item.find_all('div')[5].text
+            data['Description'].append(desc)
+        except:
+            data['Description'].append('none')
 
-    try:
-        desc = j2[i][0].find_all('div')[5].text
-        data['Description'].append(desc)
-    except:
-        data['Description'].append('none')
-
-    try:
-        stars = str(j2[i][0].find_all('div')[6]).split('"')[1].replace('\xa0', ' ')
-        if 'Stern' in stars:
-            stars = stars.split(' von')
-            stars = stars[0]
-            data['Stars'].append(stars)
-        else:
+        try:
+            stars = str(item.find_all('div')[6]).split('"')[1].replace('\xa0', ' ')
+            if 'Stern' in stars:
+                stars = stars.split(' von')
+                stars = stars[0]
+                data['Stars'].append(stars)
+            else:
+                data['Stars'].append('none')
+        except:
             data['Stars'].append('none')
-    except:
-        data['Stars'].append('none')
 
-df = pd.DataFrame(data=data)
-df.to_excel("output.xlsx")
+
+if __name__ == "__main__":
+    i = 0
+    data = {'Name': [], 'Price': [], 'Retailer': [], 'Stars': [], 'Description': []}
+    while True:
+        items = get_items(page=i)
+        i = i + 1
+        if len(items) == 0:
+            break
+        else:
+            parse_items(data, items)
+            print("Page %i" % (i))
+
+    df = pd.DataFrame(data=data)
+    df.to_excel(OUTPUT_FILE)
